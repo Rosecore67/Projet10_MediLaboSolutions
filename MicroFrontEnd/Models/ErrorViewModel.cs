c66a8401fe67a1c3c25efc71ab672e2bf6ca975b
Core Rose <cr67.rose@gmail.com>
2025-03-20 14:15:27 +0100

    Ajoutez des fichiers projet.

---
+++ MicroFrontEnd/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MicroFrontEnd.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
