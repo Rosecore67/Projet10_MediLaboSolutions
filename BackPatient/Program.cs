07b31d0ff77e9418e8d85c5f42b4e0f56fe735e7
Core Rose <cr67.rose@gmail.com>
2025-03-20 15:07:58 +0100

    V1 BackPatient : Updated Program.cs

--- BackPatient/Program.cs
+++ BackPatient/Program.cs
@@ -1,4 +1,8 @@
 using BackPatient.Data;
+using BackPatient.Repository;
+using BackPatient.Repository.Interface;
+using BackPatient.Service;
+using BackPatient.Service.Interface;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +10,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddDbContext<PatientDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IPatientRepository, PatientRepository>();
+builder.Services.AddScoped<IPatientService, PatientService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
